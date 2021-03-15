0f1b996d7d9c084fee08767ca5e495300e80d9e5
Matey-Nikolov <matey.nikolov@mail.bg>
2021-03-15 21:33:02 +0200

    ssd

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace Extract_Emails_Regex
+{
+    class Program
+    {
+        static void Main()
+        {
+            Regex regex = new Regex(@"\s(?<email>[A-Z][A-Z]*[.|_|-]?[A-Z0-9]+@[A-Z][A-Z0-9.-]*\.[A-Z]{2,})", RegexOptions.IgnoreCase);
+           
+
+            string text = Console.ReadLine();
+            //string text = "Just send email to s.miller@mit.edu and j.hopking@york.ac.uk for more information.";
+
+            MatchCollection matchesMail = regex.Matches(text);
+
+            foreach (Match extract in matchesMail)
+            {
+                Console.WriteLine(extract.Groups["email"].Value);
+            }
+        }
+    }
+}
